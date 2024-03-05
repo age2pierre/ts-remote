53c3131d66045df5599e9543b0c3cca4fa57a0bc
age2pierre <pierre.j.robillard@gmail.com>
2024-03-05 23:19:07 +0100

    feat: added typia, working client side

--- src/Comp.tsx
+++ src/Comp.tsx
@@ -1,3 +1,16 @@
+import { createIs } from "typia";
+
+type Foo = {
+  bar: number;
+};
+
+const isFoo = createIs<Foo>();
+
 export default () => {
-  return <h1>Hello world!!!</h1>;
+  return (
+    <ul>
+      <li>{`should be false => ${isFoo({ toto: 42 })}`} </li>
+      <li>{`should be true => ${isFoo({ bar: 42 })}`} </li>
+    </ul>
+  );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,25 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import typescript from "rollup-plugin-typescript2";
+import Inspect from "vite-plugin-inspect";
 // import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
+  esbuild: false,
   plugins: [
+    Inspect(),
     /* 
     Uncomment the following line to enable solid-devtools.
     For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
     */
     // devtools(),
+    { ...typescript(), enforce: "pre" },
     solidPlugin(),
   ],
   server: {
     port: 3000,
   },
   build: {
-    target: 'esnext',
+    target: "esnext",
   },
 });
